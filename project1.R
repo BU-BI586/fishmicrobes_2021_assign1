d98f97835c4fbd51ad2c42d0e4929c3e986dc367
Victoria French <Victoria1@Patricias-MacBook-Air.local>
2021-03-07 17:20:07 -0500

    Data manipulation.

--- project1.R
+++ project1.R
@@ -41,24 +41,25 @@ getwd()
 #setwd("C:/Users/Maddy/Documents/BI586/fishmicrobes_2021_assign1/fastqfiles")
 
 #path <- "/Users/gracebeery/Desktop/BI586/fishmicrobes_2021_assign1" 
-path <- "C:/Users/Maddy/Documents/BI586/fishmicrobes_2021_assign1/fastqfiles"
-path <- "/usr4/bi594/vfrench3/assignment1/fishmicrobes_2021_assign1/fastqfiles"
+#path <- "C:/Users/Maddy/Documents/BI586/fishmicrobes_2021_assign1/fastqfiles"
+#path <- "/usr4/bi594/vfrench3/assignment1/fishmicrobes_2021_assign1/fastqfiles"
+path <- "/Users/Victoria1/Desktop/Grad School/Eco Gen./fishmicrobes_2021_assignment1/fastqfiles"
 fns <- list.files(path)
-#Let's make sure that all of our files are there
 fns
 
+#Locating fastq files from current directory 
 fastqs <- fns[grepl(".fastq$", fns)]
 fastqs <- sort(fastqs) # Sort ensures reads are in same order
-fnFs <- fastqs[grepl("_1", fastqs)]
-fnRs <- fastqs[grepl("_2", fastqs)]
+fnFs <- fastqs[grepl("_1", fastqs)] #assigning forward reads to variable 
+fnRs <- fastqs[grepl("_2", fastqs)] #assigning reverse reads to variable 
 
-sample.names.F <- sapply(strsplit(fnFs, ".fastq"), `[`, 1) #the last number will select the field for renaming
-sample.names.R <- sapply(strsplit(fnRs, ".fastq"), `[`, 1) #the last number will select the field for renaming
+sample.names.F <- sapply(strsplit(fnFs, ".fastq"), `[`, 1) #renaming (simplifying file names) of forward reads 
+sample.names.R <- sapply(strsplit(fnRs, ".fastq"), `[`, 1) #renaming (simplifying file names) of reverse reads
 sample.names.F
 sample.names.R
 
 # Specify the full path to each fastq file
-fnFs <- file.path(path, fnFs)
+fnFs <- file.path(path, fnFs) 
 fnRs <- file.path(path, fnRs)
 fnFs
 fnRs
@@ -68,32 +69,30 @@ plotQualityProfile(fnFs[10:18])
 plotQualityProfile(fnRs[1:9])
 plotQualityProfile(fnRs[10:18])
 
-filt_path <- file.path(path, "trimmed")
-if(!file_test("-d", filt_path)) dir.create(filt_path)
-filtFs <- file.path(filt_path, "filteredF", paste0(sample.names.F, "_F_filt.fastq.gz"))
-filtRs <- file.path(filt_path, "filteredR", paste0(sample.names.R, "_R_filt.fastq.gz"))
+filt_path <- file.path(path, "trimmed") #assigning new trimmed folder to a variable 
+if(!file_test("-d", filt_path)) dir.create(filt_path) #creating trimmed directory 
+filtFs <- file.path(filt_path, "filteredF", paste0(sample.names.F, "_F_filt.fastq.gz")) #assigning new trimmed forward reads to variable 
+filtRs <- file.path(filt_path, "filteredR", paste0(sample.names.R, "_R_filt.fastq.gz")) #assigning new timmed reverse reads to variable 
 
-out<- filterAndTrim(fnFs, filtFs, fnRs, filtRs, truncLen=250, #set to 250 currently until Sarah responds. I think 250 works for most of the data but their are those weird outliers. 
+out<- filterAndTrim(fnFs, filtFs, fnRs, filtRs, #most samples have consistently high quality reads but a few start to taper around cycle 100; Since both forward and reverse reads, lowest Trunclen could be 125. Dropping too many reads at 125 
                      maxN=0, #DADA does not allow Ns
-                     maxEE=1, #allow 1 expected errors, where EE = sum(10^(-Q/10)); more conservative, model converges
-                     truncQ=2, 
-                     trimLeft=19, #N nucleotides to remove from the start of each read: 16S (microbial community barcoding region) V4 region (used in paper even though V2-V3 higher resolution and species level identification; https://doi.org/10.1038/sdata.2019.7) forward primer 515 F = 19
+                     maxEE=1, #allow 1 expected errors
+                     truncQ=2,  #Trimleft excluded as raw sequences from NCBI contain no primer sequences. Sequences used were: 515F and 806R for the 16S V4 region. 
                      rm.phix=TRUE, #remove reads matching phiX genome
                      compress=TRUE, multithread=FALSE) # On Windows set multithread=FALSE
 
-out <- filterAndTrim(fnFs, filtFs, fnRs, filtRs, truncLen=250,
-                     maxN=0, maxEE=1, truncQ=2, rm.phix=TRUE,
-                     compress=TRUE, multithread=TRUE)
+head(out) 
 
-head(out)
 tail(out)
 
-setDadaOpt(MAX_CONSIST=30) #usually keep 30, increase number of cycles to allow convergence
+setDadaOpt(MAX_CONSIST=30) #keep preset 30, increase number of cycles to allow convergence if data looks funny
 errF <- learnErrors(filtFs, multithread=TRUE)
 errR <- learnErrors(filtRs, multithread=TRUE)
 
-plotErrors(errF, nominalQ=TRUE) #plotting error rates, want to see mostly linear relationships
+plotErrors(errF, nominalQ=TRUE) #plotting forward read error rates, want to see mostly linear relationships
+#output: 21367840 total bases in 267098 reads from 18 samples will be used for learning the error rates.
 plotErrors(errR, nominalQ=TRUE)
+#output: 21367840 total bases in 267098 reads from 18 samples will be used for learning the error rates.
 
 #dereplicating reads - collapsing all reads into unique sequences
 derepFs <- derepFastq(filtFs, verbose=TRUE)
